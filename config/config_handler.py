08401b64e4d0dfe9da0674f54bee1787ae7836cb
MustiAbi <mustafa.uslu@stud.fra-uas.de>
2025-05-26 13:38:24 +0200

    meilenstein3version

---
+++ config/config_handler.py
@@ -0,0 +1,20 @@
+## @file network_process.py 
+## @brief Behandelt Netzwerkkommunikation (JOIN, WHO, MSG) im Chat
+
+import toml
+import os
+
+CONFIG_PATH = "config.toml"
+
+def load_config():
+
+    if not os.path.exists(CONFIG_PATH):
+
+        raise FileNotFoundError(f"Konfigurationsdatei '{CONFIG_PATH}' nicht gefunden.")
+
+        with open(CONFIG_PATH, "r", encoding="utf-8") as f:
+            return toml.load(f)
+
+
+
+
